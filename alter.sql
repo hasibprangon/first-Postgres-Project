310f79efa2a9f12c31ace7f8bc335a28414da118
hasibprangon <hasibulhossainp09@gmail.com>
2025-05-21 18:47:17 +0600

    select file added

--- alter.sql
+++ alter.sql
@@ -8,12 +8,25 @@ INSERT INTO user2 VALUES (6, 'maruf', 27, 'maruf@hotmail.com');
 -- delete column
 ALTER TABLE user2 DROP COLUMN email;
 -- rename column
-ALTER TABLE user2 RENAME COLUMN age to user_age;
+ALTER TABLE user2 RENAME COLUMN  user_age to age;
 -- type change
 ALTER TABLE user2 ALTER COLUMN user_name TYPE VARCHAR(50);
 -- add constraints
 ALTER TABLE user2 
-ALTER COLUMN user_age set NOT NULL;
+ALTER COLUMN age set NOT NULL;
 -- delete constraints
 ALTER TABLE user2 
-ALTER COLUMN user_age DROP NOT NULL;
+ALTER COLUMN age DROP NOT NULL;
+
+-- add a unique column
+ALTER TABLE user3 
+ADD CONSTRAINT unique_user2_age PRIMARY KEY(id);
+
+
+DROP Table user2;
+
+SELECT * FROM user3;
+
+INSERT INTO user3 VALUES(4, 'hridoy', 24), (5, 'maruf', 25);
+
+ALTER TABLE user3 DROP constraint unique_user2_age
---
+++ select.sql
@@ -0,0 +1,15 @@
+-- Active: 1733932584439@@127.0.0.1@5432@ph
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(100),
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(50)
+);
+
+SELECT * from students;
