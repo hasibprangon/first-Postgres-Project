7734446920101aaeb6eef6e5779532315be38295
hasibprangon <hasibulhossainp09@gmail.com>
2025-05-23 05:04:32 +0600

    delete a user whose id is foreign key on another table

--- foreignKey.sql
+++ foreignKey.sql
@@ -7,5 +7,36 @@ CREATE TABLE "user" (
 CREATE TABLE post (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
-    user_id INTEGER REFERENCES "user"(id)
-)
+    user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE
+);
+
+INSERT INTO
+    "user" (username)
+VALUES ('akash'),
+    ('batash'),
+    ('sagor'),
+    ('nodi');
+
+
+
+INSERT INTO post(title, user_id) VALUES ('Enjoying a sunny day with akash', 2),
+('Batash just shared a amazing recipe', 1),
+('Exploring adventures with sagor', 4),
+('nodi"s wisdom always leaves me inspired', 4);
+
+
+SELECT * FROM post;
+
+SELECT * FROM "user";
+
+DROP TABLE post;
+
+DROP TABLE "user";
+
+-- deletion constraints on delete user
+-- Restriction Deletion -> ON DELETE RESTRICTION / ON DELETE NO ACTION(default)
+DELETE FROM "user" WHERE id = 4;
+
+-- Cascading Deletion -> ON DELETE CASCADE
+-- Setting NULL -> ON DELETE SET NULL
+-- Set Default value -> ON DELETE SET DEFAULT
